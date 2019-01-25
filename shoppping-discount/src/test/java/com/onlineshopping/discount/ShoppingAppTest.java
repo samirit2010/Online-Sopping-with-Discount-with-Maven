bd61b8bfda55982cfef4aaeceb947a129e82dbd3
sammansu <sammansu@DIN51003503.corp.capgemini.com>
2019-01-25 17:55:36 +0530

    JUNit

--- shoppping-discount/src/test/java/com/onlineshopping/discount/ShoppingAppTest.java
+++ shoppping-discount/src/test/java/com/onlineshopping/discount/ShoppingAppTest.java
@@ -1,5 +1,13 @@
 package com.onlineshopping.discount;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+
+import com.onlineshopping.products.Product;
+
 public class ShoppingAppTest {
 
 	protected void setUp() throws Exception {
@@ -8,4 +16,11 @@ public class ShoppingAppTest {
 	        System.out.println("before");  
 		}
 	
+	@Test
+	public void testProdut() {
+		Product	product1 = new Product("pen","grocerry",50);
+		assertEquals("Set Product Name :", "pen", product1.getProductName()); 
+		assertEquals("Set Product Type :", "grocerry", product1.getProductType()); 
+		assertEquals("Set Product Price :", 50, product1.getPrice());
+	}
 }
